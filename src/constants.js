8fa38c23c35f878aef738919fe91a9dd9880a0be
Abdualrahman <161183187+dhmii0@users.noreply.github.com>
2024-06-01 09:20:08 +0300

    First Commit

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript these days.
+    This is my first game. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/dhmii04" target="_blank">Github</a>!`,
+  "swe-degree": `I'm gonna put my SWE degree here.`,
+  "sofa-table": `That's my sofa. I like to chill here and watch YouTube.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This is where I sleep. I often come up with great ideas while lying in bed. When an idea strikes, I usually need to write it down; otherwise, I can't sleep because my mind keeps dwelling on it.`,
+  resume: `This is my desk and on it, I'm going to put my resume when I make one.
+  Contact me at abdualrahmanalghamdi3@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves. There is even one in Arabic.
+  I've likely only read one of them. Who else has the habit of buying technical books but never finishes them?`,
+  "sofa-table2": `I like to read books on this sofa.`,
+  exit: `If you want to exit Abdualrahman's portfolio, just close the tab.`,
+};
